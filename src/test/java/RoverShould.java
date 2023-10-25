13a7d12acf8c505976d66567eb2e9a6c9ee05ef4
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-10-25 10:59:45 +0200

    :recycle: Extract variable initialPosition and initialDirection

--- src/test/java/RoverShould.java
+++ src/test/java/RoverShould.java
@@ -5,11 +5,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class RoverShould {
     @Test
     void start_with_and_initial_position_facing_initial_direction () {
+        Position initialPosition = new Position(0, 0);
+        North initialDirection = new North();
+        Rover rover = new Rover(initialPosition, initialDirection);
 
-        Rover rover = new Rover(new Position(0,0), new North());
-
-        assertThat(rover.getPosition()).isEqualTo(new Position(0,0));
-        assertThat(rover.getDirection()).isEqualTo(new North());
+        assertThat(rover.getPosition()).isEqualTo(initialPosition);
+        assertThat(rover.getDirection()).isEqualTo(initialDirection);
     }
 }
 
