7f5f3d3228cd2f1ae1cc8f77f17096d3313757f0
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-10-25 11:37:03 +0200

    :recycle: :Add @BeforeEach in RoverShould class

--- src/test/java/RoverShould.java
+++ src/test/java/RoverShould.java
@@ -1,25 +1,27 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RoverShould {
-    @Test
-    void start_with_and_initial_position_facing_initial_direction () {
-        Position initialPosition = new Position(0, 0);
-        North initialDirection = new North();
-        Rover rover = new Rover(initialPosition, initialDirection);
+    Position initialPosition;
+    Direction initialDirection;
+    Rover rover;
+    @BeforeEach
+    void setup() {
+        initialPosition = new Position(0, 0);
+        initialDirection = new North();
+        rover = new Rover(initialPosition, initialDirection);
+    }
 
+    @Test
+    void start_with_and_initial_position_facing_initial_direction() {
         assertThat(rover.getPosition()).isEqualTo(initialPosition);
         assertThat(rover.getDirection()).isEqualTo(initialDirection);
     }
     @Test
-    void turn_left () {
-        Position initialPosition = new Position(0, 0);
-        North initialDirection = new North();
-        Rover rover = new Rover(initialPosition, initialDirection);
-
+    void turn_left() {
         rover.turnLeft();
-
         assertThat(rover.getDirection()).isEqualTo(new West());
     }
 }
