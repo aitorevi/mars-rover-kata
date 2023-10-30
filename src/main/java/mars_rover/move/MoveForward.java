9a47beec25b8dcc9f9d1ddc5ce2f5b83df5ca933
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-10-30 11:50:54 +0100

    :recycle: Extract method toNorth

--- src/main/java/mars_rover/move/MoveForward.java
+++ src/main/java/mars_rover/move/MoveForward.java
@@ -7,10 +7,15 @@ import mars_rover.direction.*;
 public record MoveForward(Direction direction, Territory territory) implements Move {
     public Position execute(Position position) {
         return switch (direction) {
-            case North n -> territory.topLimit() == position.y() ? new Position(position.x(),0) : position.incrementY();
+            case North n -> toNorth(position);
             case West w -> position.decrementX();
             case South s -> position.decrementY();
             case East e -> position.incrementX();
         };
+    }
+
+    private Position toNorth(Position position) {
+        boolean isInTheTopLimit = territory.topLimit() == position.y();
+        return isInTheTopLimit ? new Position(position.x(), 0) : position.incrementY();
     }
 }
