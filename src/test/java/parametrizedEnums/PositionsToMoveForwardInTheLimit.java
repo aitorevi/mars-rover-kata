05d1f8e8db102ea88ff35c6eb59517ef7b6bdf15
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-10-30 14:03:25 +0100

    :recycle: Rename all posibles limits in position to move forward in the limit

--- src/test/java/parametrizedEnums/PositionsToMoveForwardInTheLimit.java
+++ src/test/java/parametrizedEnums/PositionsToMoveForwardInTheLimit.java
@@ -4,14 +4,14 @@ import mars_rover.direction.*;
 import mars_rover.position.Position;
 
 public enum PositionsToMoveForwardInTheLimit {
-    NORTH(new Position(1, 2), new North(), new Position(1,0)),
-    SOUTH(new Position(1, 0), new South(), new Position(1,2)),
-    EAST(new Position(2, 1), new East(), new Position(0,1)),
-    WEST(new Position(0, 1), new West(), new Position(2,1));
+    TOP(new Position(1, 2), new North(), new Position(1,0)),
+    BOTTOM(new Position(1, 0), new South(), new Position(1,2)),
+    RIGHT(new Position(2, 1), new East(), new Position(0,1)),
+    LEFT(new Position(0, 1), new West(), new Position(2,1));
 
-    public Position initialPosition;
-    public Direction direction;
-    public Position expectedPosition;
+    public final Position initialPosition;
+    public final Direction direction;
+    public final Position expectedPosition;
 
     PositionsToMoveForwardInTheLimit(Position initialPosition, Direction direction, Position expectedPosition) {
         this.initialPosition = initialPosition;
