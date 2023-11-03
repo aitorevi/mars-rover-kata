5e32e3632c1280c304ee71bb070fbe10bf01d674
Sstark97 <aitor@leanmind.es>
2023-11-03 12:59:26 +0000

    Test(:rotating_light:): TerritoryShould check that y axis be greater than 0

--- src/test/java/TerritoryShould.java
+++ src/test/java/TerritoryShould.java
@@ -7,8 +7,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 public class TerritoryShould {
     @Test
     void check_that_x_axis_be_greater_than_0 () {
-
-        assertThatThrownBy(() -> new Territory(0, 2)).isInstanceOf(IlegalDimensionSizeException.class)
-                .hasMessageContaining("The dimension must be greater than 0");
+        assertThatThrownBy(() -> new Territory(0, 2))
+            .isInstanceOf(IlegalDimensionSizeException.class)
+            .hasMessageContaining("The dimension must be greater than 0");
+    }
+    @Test
+    void check_that_y_axis_be_greater_than_0 () {
+        assertThatThrownBy(() -> new Territory(2, 0))
+            .isInstanceOf(IlegalDimensionSizeException.class)
+            .hasMessageContaining("The dimension must be greater than 0");
     }
 }
