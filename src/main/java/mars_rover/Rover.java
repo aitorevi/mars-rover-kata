076214d7a1da0c5a2f2988cd6cc491e1c82117ec
Sstark97 <aitorscinfo@gmail.com>
2023-12-15 12:28:05 +0000

    :recycle: Remove this keyword in Rover

--- src/main/java/mars_rover/Rover.java
+++ src/main/java/mars_rover/Rover.java
@@ -3,48 +3,21 @@ package mars_rover;
 import mars_rover.command.Command;
 
 import java.util.List;
-import java.util.Objects;
 
-public class Rover {
-    private final Driver driver;
-
-    public Rover(Driver driver) {
-        this.driver = driver;
-    }
-
+public record Rover(Driver driver) {
     public Rover followThis(List<Command> commands) {
         for (Command command: commands) {
             executeCommand(command);
         }
-        return new Rover(this.driver);
+        return new Rover(driver);
     }
 
     private void executeCommand(Command command) {
         switch (command) {
-            case FORWARD -> this.driver.moveForward();
-            case BACKWARD -> this.driver.moveBackward();
-            case TURN_LEFT -> this.driver.turnLeft();
-            case TURN_RIGHT -> this.driver.turnRight();
+            case FORWARD -> driver.moveForward();
+            case BACKWARD -> driver.moveBackward();
+            case TURN_LEFT -> driver.turnLeft();
+            case TURN_RIGHT -> driver.turnRight();
         }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Rover rover = (Rover) o;
-        return Objects.equals(driver, rover.driver);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(driver);
-    }
-
-    @Override
-    public String toString() {
-        return "Rover{" +
-                "driver=" + driver +
-                '}';
     }
 }
