257aba25dbecb9c3e324db81e6048734fefdc125
Aitor Reviriego Amor <aitorevi1979@gmail.com>
2026-04-20 22:51:11 +0200

    :sparkles: Add Spring Boot scaffold (gradle plugin, main class, application.properties)

---
+++ src/main/java/mars_rover/app/MarsRoverApplication.java
@@ -0,0 +1,11 @@
+package mars_rover.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsRoverApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MarsRoverApplication.class, args);
+    }
+}
