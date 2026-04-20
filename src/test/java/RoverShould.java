2b1aac95f84610d7e79a53df97255fa3e7785c4f
Aitor Reviriego Amor <aitorevi1979@gmail.com>
2026-04-20 22:49:54 +0200

    :white_check_mark: Rover aborts sequence when driver is blocked

--- src/test/java/RoverShould.java
+++ src/test/java/RoverShould.java
@@ -1,8 +1,11 @@
 import builders.DriverBuilder;
 import builders.RoverBuilder;
+import builders.TerritoryBuilder;
 import mars_rover.Driver;
 import mars_rover.Rover;
+import mars_rover.Territory;
 import mars_rover.command.Command;
+import mars_rover.direction.North;
 import mars_rover.direction.South;
 import mars_rover.position.Position;
 import org.junit.jupiter.api.Test;
@@ -24,5 +27,27 @@ public class RoverShould {
                 .build();
 
         assertThat(roverAfterCommands).isEqualTo(new RoverBuilder().withDriver(expectedDriver).build());
+    }
+
+    @Test
+    void stop_before_the_obstacle_and_report_it() {
+        Position obstacle = new Position(1, 3);
+        Territory territory = new TerritoryBuilder()
+                .withXLimit(5)
+                .withYLimit(5)
+                .withObstacles(List.of(obstacle))
+                .build();
+        Driver driver = new DriverBuilder()
+                .withPosition(new Position(1, 1))
+                .withDirection(new North())
+                .withTerritory(territory)
+                .build();
+        Rover rover = new RoverBuilder().withDriver(driver).build();
+
+        Rover finalRover = rover.followThis(List.of(FORWARD, FORWARD, FORWARD, TURN_RIGHT));
+
+        org.assertj.core.api.Assertions.assertThat(finalRover.driver().getPosition()).isEqualTo(new Position(1, 2));
+        org.assertj.core.api.Assertions.assertThat(finalRover.driver().getDirection()).isInstanceOf(North.class);
+        org.assertj.core.api.Assertions.assertThat(finalRover.obstacleHit()).contains(obstacle);
     }
 }
